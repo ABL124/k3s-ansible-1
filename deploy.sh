e31eaa36e3db2588df4ef883c6e4a062c831070e
ABL124 <codwarrior83@gmail.com>
2022-09-03 21:51:04 +0000

    First install

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook site.yml -i inventory/my-cluster/hosts.ini
+ansible-playbook site.yml -i inventory/k3s/hosts.ini --key-file "~/.ssh/ansible" --ask-become-pass
--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook reset.yml -i inventory/my-cluster/hosts.ini
+ansible-playbook reset.yml -i inventory/k3s/hosts.ini --key-file "~/.ssh/ansible" --ask-become-pass
